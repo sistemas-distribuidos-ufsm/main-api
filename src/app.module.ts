4b28f284c5efc8cbb04b2b8470f65c330ff5ba3a
Leonardo Trindade <ltrindade@inf.ufsm.br>
2022-11-07 17:10:14 -0300

    Add environment variables support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HelloWorldController } from './modules/hello-world/hello-world.controller';
 import { HelloWorldService } from './modules/hello-world/hello-world.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+  ],
   controllers: [HelloWorldController],
   providers: [HelloWorldService],
 })
