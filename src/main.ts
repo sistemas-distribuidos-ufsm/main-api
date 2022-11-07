43ef75982d27496d3b8bb87963c6639645a85a98
Leonardo Trindade <ltrindade@inf.ufsm.br>
2022-11-06 21:43:43 -0300

    Added swagger to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('API Title')
+    .setDescription('Routes and descriptions of API Title')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const port = process.env.PORT || 3000; // TODO: Adjust .env package and create files
+
+  await app.listen(port, () => console.log(`API Title started at ${port}`));
 }
 bootstrap();
