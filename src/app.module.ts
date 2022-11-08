10138e56cb3797fd0bca09abfc96d878f7067580
Leonardo Trindade <ltrindade@inf.ufsm.br>
2022-11-08 10:38:20 -0300

    Adds Sequelize ORM and their configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { DatabaseModels } from './database/migrations/database.models';
 import { HelloWorldController } from './modules/hello-world/hello-world.controller';
 import { HelloWorldService } from './modules/hello-world/hello-world.service';
 
@@ -8,6 +10,15 @@ import { HelloWorldService } from './modules/hello-world/hello-world.service';
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [...DatabaseModels],
     }),
   ],
   controllers: [HelloWorldController],
