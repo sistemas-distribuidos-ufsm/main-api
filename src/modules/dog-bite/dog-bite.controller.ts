dab7a7d4feb965c6959c80eb6e449e3805188114
Leonardo Trindade <ltrindade@inf.ufsm.br>
2022-11-20 18:21:01 -0300

    WIP: adds crud methods to dog bite controller

--- src/modules/dog-bite/dog-bite.controller.ts
+++ src/modules/dog-bite/dog-bite.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { DogBiteService } from './dog-bite.service';
+import { CreateDogBiteDto } from './dto/create-dog-bite.dto';
+import { DogBite } from './entities/dog-bite.entity';
 
 @Controller('dog-bite')
-export class DogBiteController {}
+export class DogBiteController {
+  constructor(private dogBiteService: DogBiteService) {}
+  @Post()
+  async create(data: CreateDogBiteDto): Promise<DogBite> {
+    return this.dogBiteService.create(data);
+  }
+
+  @Get()
+  async findAll(): Promise<DogBite[]> {
+    return this.dogBiteService.findAll();
+  }
+
+  @Get(':dogBiteId')
+  async findById(@Param('dogBiteId') dogBiteId: number): Promise<DogBite> {
+    return this.dogBiteService.findById(dogBiteId);
+  }
+
+  @Delete(':dogBiteId')
+  async delete(@Param('dogBiteId') dogBiteId: number): Promise<DogBite> {
+    return this.dogBiteService.delete(dogBiteId);
+  }
+}
--- src/modules/dog-bite/dog-bite.service.ts
+++ src/modules/dog-bite/dog-bite.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateDogBiteDto } from './dto/create-dog-bite.dto';
+import { DogBite } from './entities/dog-bite.entity';
 
 @Injectable()
-export class DogBiteService {}
+export class DogBiteService {
+  constructor(@InjectModel(DogBite) private dogBiteModel: typeof DogBite) {}
+
+  async create(data: CreateDogBiteDto): Promise<DogBite> {
+    try {
+      const dogBite = await this.dogBiteModel.create(data);
+
+      return dogBite;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async findAll(): Promise<DogBite[]> {
+    try {
+      const dogBites = await this.dogBiteModel.findAll();
+
+      return dogBites;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async findById(id: number): Promise<DogBite> {
+    try {
+      const dogBite = await this.dogBiteModel.findByPk(id);
+
+      if (!dogBite) {
+        throw new NotFoundException('Dog bite not found');
+      }
+
+      return dogBite;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async delete(id: number): Promise<DogBite> {
+    try {
+      console.log('WIP');
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+}
