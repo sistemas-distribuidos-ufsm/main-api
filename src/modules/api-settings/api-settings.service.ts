af1ea290acca3b0b3ebbbce84e7084419f858598
Leonardo Trindade <ltrindade@inf.ufsm.br>
2023-01-24 00:51:30 -0300

    feat: adjusted check all apis status to check who is working

--- src/modules/api-settings/api-settings.service.ts
+++ src/modules/api-settings/api-settings.service.ts
@@ -1,36 +1,45 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, ServiceUnavailableException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ApiSettingsService {
   private readonly API_URLS = [
-    'https://viacep.com.br/ws/97105230/json/',
-    'https://viacep.com.br/ws/97105200/json/',
-    'https://viacep.com.br/ws/97300000/json/',
+    'http://localhost:3001/api-settings/status',
+    'http://localhost:3002/api-settings/status',
+    'http://localhost:3003/api-settings/status',
+    'http://localhost:3004/api-settings/status',
   ];
 
   constructor(private readonly httpService: HttpService) {}
 
   async getStatus(): Promise<any> {
-    try {
-      const responses = [];
+    const responses = [];
 
-      for (const url of this.API_URLS) {
-        const request = this.httpService.get(url).pipe(
-          map((response) => {
-            return { status: response?.status, data: response?.data };
-          }),
-        );
+    for (const url of this.API_URLS) {
+      const request = this.httpService.get(url).pipe(
+        map((response) => {
+          return {
+            api: url,
+            status: response?.status,
+            data: response?.data,
+          };
+        }),
+      );
 
+      try {
         const response = await lastValueFrom(request);
         responses.push(response);
+      } catch (error) {
+        responses.push({
+          api: url,
+          status: 502,
+          data: `Unavaiable - ${error.message}`,
+        });
       }
-
-      return responses;
-    } catch (error) {
-      throw new ServiceUnavailableException(error.message);
     }
+
+    return responses;
   }
 }
